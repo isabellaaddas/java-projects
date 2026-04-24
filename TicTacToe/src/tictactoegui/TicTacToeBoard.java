7f4e9725d9f67fabf5988ead0d755ca62923d900
isabellaaddas <isabellaaddas@gmail.com>
2026-04-24 18:29:08 -0400

    started setting up java swing implementation

--- TicTacToe/src/tictactoegui/TicTacToeBoard.java
+++ TicTacToe/src/tictactoegui/TicTacToeBoard.java
@@ -1,4 +1,24 @@
 package tictactoegui;
+import javax.swing.*;
 
+/*
+ * This class will be the board where everything happens for
+ * a game of Tic Tac Toe.
+ */
 public class TicTacToeBoard {
+
+    public static void main(String[] args) {
+        // Use invoke later and create runnable method to
+        // handle the single thread where everything runs
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                {
+                    JFrame window = new JFrame();
+                    window.setTitle("Tic-Tac-Toe");
+
+                }
+            }
+        });
+    }
 }
