80cea96813744dbdd5b4fc9c146cc58c98612ce6
isabellaaddas <isabellaaddas@gmail.com>
2026-04-26 10:47:51 -0400

    deleted GameWindow class; TicTacToeGUI handles the GUI and logic of the game while TicTacToeBoard handles the logic of the game and what to show

--- TicTacToe/src/tictactoegui/TicTacToeBoard.java
+++ TicTacToe/src/tictactoegui/TicTacToeBoard.java
@@ -1,24 +1,57 @@
 package tictactoegui;
+
 import javax.swing.*;
+import java.awt.*;
 
 /*
- * This class will be the board where everything happens for
- * a game of Tic Tac Toe.
+ * Separate class will handle the board where everything
+ * related to the game (showing the board, updating the board,
+ * etc.) must happen while the TicTacToeBoardGUI class
+ * handles logic to invoke those actions.
  */
 public class TicTacToeBoard {
+    private JFrame window;
+    private JPanel panel;
 
-    public static void main(String[] args) {
-        // Use invoke later and create runnable method to
-        // handle the single thread where everything runs
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                // Create instance GameWindow
-                GameWindow game = new GameWindow();
+    // Create instance of the game window with some options
+    // to handle default closing, size, etc.
+    public TicTacToeBoard() {
+        // JFrame window settings
+        window = new JFrame();
+        window.setTitle("Tic-Tac-Toe");
+        window.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        window.setSize(500, 500);
+        window.setLocationRelativeTo(null);
 
-                // Immediately show the window
-                game.showWindow();
-            }
-        });
+        // JPanel settings
+        panel = new JPanel();
+        panel.setLayout(new GridLayout(3, 3, 5, 5));
+        panel.setBackground(Color.BLACK);
+        setGrid(panel);
+
+        window.add(panel);
+    }
+
+    // Running this method shows the window with the
+    // board
+    public void showWindow() {
+        window.setVisible(true);
+    }
+
+    /* Private methods */
+
+    // This method will take a given panel and
+    // populate it with panels for the grid matrix
+    // that will be the TTT game board
+    private void setGrid(JPanel p) {
+        // Create new Dimension object that will be the
+        // size of each cell
+        Dimension size = new Dimension(15, 15);
+        for (int i = 0; i < 9; i++) {
+            JPanel cell = new JPanel();
+            cell.setBackground(Color.ORANGE);
+            cell.setPreferredSize(size);
+            p.add(cell);
+        }
     }
 }
