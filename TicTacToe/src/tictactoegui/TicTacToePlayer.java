192a9bc60646f7403c21f3da02e8de5ea9ca43b7
isabellaaddas <isabellaaddas@gmail.com>
2026-04-28 16:21:28 -0400

    implemented TicTacToePlayer abstract class with data members for subclasses, concrete getter methods, and abstract setter methods

--- TicTacToe/src/tictactoegui/TicTacToePlayer.java
+++ TicTacToe/src/tictactoegui/TicTacToePlayer.java
@@ -1,4 +1,28 @@
 package tictactoegui;
 
-public class TicTacToePlayer {
+import javax.swing.*;
+
+public abstract class TicTacToePlayer {
+    private char player;
+    private ImageIcon playerIcon;
+
+    // This method will retrieve the icon object
+    // representing the player's icon
+    public ImageIcon getPlayerIcon() {
+        return playerIcon;
+    }
+
+    // This method will retrieve the char variable
+    // representing which player the instance is
+    public char getPlayer() {
+        return player;
+    }
+
+    // This method will set the icon based on which
+    // player (x or o) the object instance is
+    abstract void setPlayerIcon();
+
+    // This method will set the char variable for an
+    // instance of a player subclass
+    abstract void setPlayer();
 }
