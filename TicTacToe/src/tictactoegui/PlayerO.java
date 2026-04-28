9c5d0a8b8db183983822c36c1a7db3ff21ed5710
isabellaaddas <isabellaaddas@gmail.com>
2026-04-28 16:46:43 -0400

    extended PlayerX and PlayerO classes from abstract parent with appropriate constructor

--- TicTacToe/src/tictactoegui/PlayerO.java
+++ TicTacToe/src/tictactoegui/PlayerO.java
@@ -1,4 +1,11 @@
 package tictactoegui;
 
-public class PlayerO {
+import javax.swing.*;
+
+public class PlayerO extends TicTacToePlayer {
+    // Implement the constructor with 'o' and the
+    // proper path to the o icon
+    PlayerO(char p, String path) {
+        super(p, path);
+    }
 }
--- TicTacToe/src/tictactoegui/PlayerX.java
+++ TicTacToe/src/tictactoegui/PlayerX.java
@@ -3,6 +3,9 @@ package tictactoegui;
 import javax.swing.*;
 
 public class PlayerX extends TicTacToePlayer {
-
-    // Implement
+    // Implement the constructor with 'x' and the
+    // proper path to the x icon
+    PlayerX(char p, String path) {
+        super(p, path);
+    }
 }
