cd3d6113129f3f58b3bb10e954c4e3508c916368
isabellaaddas <isabellaaddas@gmail.com>
2026-06-05 15:04:33 -0400

    removed mouse listener from GUI invokeLater() thread

--- TicTacToe/src/tictactoegui/TicTacToeGUI.java
+++ TicTacToe/src/tictactoegui/TicTacToeGUI.java
@@ -11,10 +11,6 @@ import java.awt.event.MouseEvent;
 public class TicTacToeGUI {
 
     public static void main(String[] args) {
-        // Set a boolean variable to handle which
-        // player turn is on
-        final boolean[] xTurn = {true};
-
         // Use invoke later and create runnable method to
         // handle the single thread where everything runs
         SwingUtilities.invokeLater(new Runnable() {
@@ -25,32 +21,6 @@ public class TicTacToeGUI {
 
                 // Immediately show the window
                 game.showWindow();
-
-                // Add a mouse event listener for the entire
-                // board to invoke the makeMove() method,
-                // which will automatically handle the game
-                // state upon being finished
-                game.getPanel().addMouseListener(new MouseAdapter() {
-                    @Override
-                    public void mouseClicked(MouseEvent e) {
-                        // Only make a move if the object that
-                        // was clicked is a panel object, aka
-                        // a cell on the board
-                        if (e.getSource() instanceof JPanel) {
-                            JPanel cellClicked = (JPanel) e.getSource();
-                            System.out.println("Cell: " + cellClicked.getName());
-                            // If current turn is for player x,
-                            // move will be registered with x char
-                            if (xTurn[0]) {
-                                game.makeMove(cellClicked, 'x');
-                                xTurn[0] = !xTurn[0];
-                            } else {
-                                game.makeMove(cellClicked, 'o');
-                                xTurn[0] = !xTurn[0];
-                            }
-                        }
-                    }
-                });
             }
         });
     }
