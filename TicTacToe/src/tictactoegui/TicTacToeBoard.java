d3c0382a3f49d33383de95971328a4fb69cdd25c
isabellaaddas <isabellaaddas@gmail.com>
2026-06-05 15:09:31 -0400

    imported mouse adapter and mouse event (bypassing implementing all five methods of mouse listener

--- TicTacToe/src/tictactoegui/TicTacToeBoard.java
+++ TicTacToe/src/tictactoegui/TicTacToeBoard.java
@@ -2,8 +2,8 @@ package tictactoegui;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
